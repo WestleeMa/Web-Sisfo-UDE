d4c0a38d16978b577dacf6df435f1aac50894d38
WestleeMa <115126072+WestleeMa@users.noreply.github.com>
2024-04-09 22:24:45 +0700

    update dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/dashboard";
 // import "animate.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -4,7 +4,12 @@ const Dashboard = () => {
   return (
     <>
       <Navbar />
-      <p>halo</p>
+      <div className="container mx-auto px-4 py-4">
+        {/* Link Card */}
+        <div className="w-[10rem] h-[10rem] bg-white rounded-md">
+          <div className="p-[1rem] bg-sky-500 rounded-md">Links</div>
+        </div>
+      </div>
     </>
   );
 };
