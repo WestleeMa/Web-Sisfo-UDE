1241cd6f158775ef4c4634b2449a03ce75ba292f
WestleeMa <115126072+WestleeMa@users.noreply.github.com>
2024-05-05 00:16:22 +0700

    update tabel admin

--- src/components/TabelInformasi.jsx
+++ src/components/TabelInformasi.jsx
@@ -1,7 +1,94 @@
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  Image,
+  Button,
+} from "@nextui-org/react";
+
+import testIMG from "../assets/images.jpg";
+
 export default function MngInfo() {
   return (
     <>
-      <p>Test</p>
+      <Table aria-label="Example static collection table">
+        <TableHeader>
+          <TableColumn>ID</TableColumn>
+          <TableColumn>Title</TableColumn>
+          <TableColumn>Description</TableColumn>
+          <TableColumn>Image</TableColumn>
+          <TableColumn>Action</TableColumn>
+        </TableHeader>
+        <TableBody>
+          <TableRow key="1">
+            <TableCell>1</TableCell>
+            <TableCell>Title 1</TableCell>
+            <TableCell>Desc 1</TableCell>
+            <TableCell>
+              <Image src={testIMG}></Image>
+            </TableCell>
+            <TableCell>
+              <Button color="warning" variant="shadow" className="mx-3">
+                Edit
+              </Button>
+              <Button color="danger" variant="shadow" className="mx-3">
+                Delete
+              </Button>
+            </TableCell>
+          </TableRow>
+          <TableRow key="2">
+            <TableCell>2</TableCell>
+            <TableCell>Title 2</TableCell>
+            <TableCell>Desc 2</TableCell>
+            <TableCell>
+              <Image src={testIMG}></Image>
+            </TableCell>
+            <TableCell>
+              <Button color="warning" variant="shadow" className="mx-3">
+                Edit
+              </Button>
+              <Button color="danger" variant="shadow" className="mx-3">
+                Delete
+              </Button>
+            </TableCell>
+          </TableRow>
+          <TableRow key="3">
+            <TableCell>3</TableCell>
+            <TableCell>Title 3</TableCell>
+            <TableCell>Desc 3</TableCell>
+            <TableCell>
+              <Image src={testIMG}></Image>
+            </TableCell>
+            <TableCell>
+              <Button color="warning" variant="shadow" className="mx-3">
+                Edit
+              </Button>
+              <Button color="danger" variant="shadow" className="mx-3">
+                Delete
+              </Button>
+            </TableCell>
+          </TableRow>
+          <TableRow key="4">
+            <TableCell>4</TableCell>
+            <TableCell>Title 4</TableCell>
+            <TableCell>Desc 4</TableCell>
+            <TableCell>
+              <Image src={testIMG}></Image>
+            </TableCell>
+            <TableCell>
+              <Button color="warning" variant="shadow" className="mx-3">
+                Edit
+              </Button>
+              <Button color="danger" variant="shadow" className="mx-3">
+                Delete
+              </Button>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </>
   );
 }
--- src/components/TabelThesis.jsx
+++ src/components/TabelThesis.jsx
@@ -1,7 +1,83 @@
+import {
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from "@nextui-org/react";
+
 export default function MngThesis() {
   return (
     <>
-      <p>Test2</p>
+      <Table aria-label="Example static collection table">
+        <TableHeader>
+          <TableColumn>ID</TableColumn>
+          <TableColumn>Bidang Kajian</TableColumn>
+          <TableColumn>Dosen Pembimbing</TableColumn>
+          <TableColumn>Judul</TableColumn>
+          <TableColumn>Action</TableColumn>
+        </TableHeader>
+        <TableBody>
+          <TableRow key="1">
+            <TableCell>1</TableCell>
+            <TableCell>CREW</TableCell>
+            <TableCell>Ira Rasikawati, Ph.D.</TableCell>
+            <TableCell>Judul 1</TableCell>
+            <TableCell>
+              <Button color="warning" variant="shadow" className="mx-3">
+                Details
+              </Button>
+              <Button color="danger" variant="shadow" className="mx-3">
+                Delete
+              </Button>
+            </TableCell>
+          </TableRow>
+          <TableRow key="2">
+            <TableCell>2</TableCell>
+            <TableCell>TESOL</TableCell>
+            <TableCell>Ignasia Yuyun, S.Pd., M.Pd.</TableCell>
+            <TableCell>Judul 2</TableCell>
+            <TableCell>
+              <Button color="warning" variant="shadow" className="mx-3">
+                Details
+              </Button>
+              <Button color="danger" variant="shadow" className="mx-3">
+                Delete
+              </Button>
+            </TableCell>
+          </TableRow>
+          <TableRow key="3">
+            <TableCell>3</TableCell>
+            <TableCell>TIN</TableCell>
+            <TableCell>Hanna Juliaty, S.S., M.A.</TableCell>
+            <TableCell>Judul 3</TableCell>
+            <TableCell>
+              <Button color="warning" variant="shadow" className="mx-3">
+                Details
+              </Button>
+              <Button color="danger" variant="shadow" className="mx-3">
+                Delete
+              </Button>
+            </TableCell>
+          </TableRow>
+          <TableRow key="4">
+            <TableCell>4</TableCell>
+            <TableCell>TESOL</TableCell>
+            <TableCell>Didi Sulistiyono, S.s., M.Hum.</TableCell>
+            <TableCell>Judul 4</TableCell>
+            <TableCell>
+              <Button color="warning" variant="shadow" className="mx-3">
+                Details
+              </Button>
+              <Button color="danger" variant="shadow" className="mx-3">
+                Delete
+              </Button>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </>
   );
 }
